3943efe1b858548349ad3f5bf31304bf9ae69287
Peenapat Jangjai <parkjj2560@gmail.com>
2025-12-10 22:16:01 +0700

    cerate subscribe for notificaition

--- backend/src/modules/push-log/push-log.module.ts
+++ backend/src/modules/push-log/push-log.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PushLogService } from './push-log.service';
 import { PushLogController } from './push-log.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PushLog } from './entities/push-log.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PushLog])],
   controllers: [PushLogController],
   providers: [PushLogService],
 })
-export class PushLogModule {}
+export class PushLogModule { }
